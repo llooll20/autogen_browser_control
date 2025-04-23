b6ee893ed272f5c37e072ead852762c39b074039
llooll20 <andgang@naver.com>
2025-04-23 17:57:42 +0900

    mcp+stt 연결
    1. 네이버 api를 사용하는 mcp를 autogen과 연결하였다.
    2. 로컬 음성데이터를 stt서버로 보내고 텍스트로 변환된 값을 받아 사용하는 것 추가
    3. 텍스트 데이터를 받아 이를 사용하는 부분 추가
    
    현재 mcp, autogen 자체 서버는 로컬로 설정되어있음

--- .venv/main.py
+++ .venv/main.py
@@ -7,13 +7,78 @@ from autogen_ext.models.openai import OpenAIChatCompletionClient
 from autogen_agentchat.ui import Console
 from autogen_ext.agents.web_surfer import MultimodalWebSurfer
 
+from autogen_core import CancellationToken
+from autogen_ext.tools.mcp import SseMcpToolAdapter, SseServerParams
+
 from dotenv import load_dotenv
 import os
+import requests
+from fastapi import FastAPI, Request
 
 load_dotenv()
 API_KEY = os.getenv("OPENAI_API_KEY")
-    
-async def main() -> None:
+
+#stt데이터 주소 (태훈이 집컴)
+stt_url = 'https://e169-39-122-179-149.ngrok-free.app/stt'
+
+app = FastAPI()
+
+#텍스트를 통해 에이전트 사용
+@app.post("/receive-text")
+async def receive_text(request: Request):
+    command= (await request.json())['text']
+    print(command)
+    await main(command)
+    return {"message": "successfully get text"," text:":command}
+
+#음성을 통해 에이전트 사용
+@app.post("/receive-voice")
+async def receive_voice() -> None:
+    # 업로드할 음성파일 경로
+    file_path = 'audio.wav'
+
+    # 파일 열기
+    with open(file_path, 'rb') as f:
+        files = {'file': ('audio.wav', f, 'audio/wav')}
+        response = requests.post(stt_url, files=files)
+
+    # 응답 처리
+    if response.status_code == 200:
+        data = response.json()
+        command = data.get('text')
+        print("받은 텍스트:", command)
+    else:
+        print("요청 실패:", response.status_code)
+        print("응답 내용:", response.text)
+    await main(command)
+
+#에이전트 실행 코드
+async def main(command) -> None:
+    #mcp 서버 연결
+    server_params = SseServerParams(
+        #mcp 서버에서 만든 로컬 url
+        url="http://127.0.0.1:8000/sse",
+        headers={"Content-Type":"application/json"},
+        timeout=30,
+    )
+
+    #mcp 서버의 도구
+    adapter_weather= await SseMcpToolAdapter.from_server_params(server_params,"get_weather")
+    adapter_blog= await SseMcpToolAdapter.from_server_params(server_params,"search_blog")
+    adapter_news= await SseMcpToolAdapter.from_server_params(server_params,"search_news")
+    adapter_book= await SseMcpToolAdapter.from_server_params(server_params,"search_book")
+    adapter_book_adv= await SseMcpToolAdapter.from_server_params(server_params,"get_book_adv")
+    adapter_encyc= await SseMcpToolAdapter.from_server_params(server_params,"search_encyc")
+    adapter_cafe= await SseMcpToolAdapter.from_server_params(server_params,"search_cafe_article")
+    adapter_kin= await SseMcpToolAdapter.from_server_params(server_params,"search_kin")
+    adapter_local= await SseMcpToolAdapter.from_server_params(server_params,"search_local")
+    adapter_spelling= await SseMcpToolAdapter.from_server_params(server_params,"fix_spelling")
+    adapter_webkr= await SseMcpToolAdapter.from_server_params(server_params,"search_webkr")
+    adapter_image= await SseMcpToolAdapter.from_server_params(server_params,"search_image")
+    adapter_shop= await SseMcpToolAdapter.from_server_params(server_params,"search_shop")
+    adapter_doc= await SseMcpToolAdapter.from_server_params(server_params,"search_doc")
+
+    #ai 모델 연결
     model_client = OpenAIChatCompletionClient(model="gpt-4o", api_key = API_KEY)
     # The web surfer will open a Chromium browser window to perform web browsing tasks.
     web_surfer = MultimodalWebSurfer("web_surfer", model_client, description="""A helpful assistant with access to a web browser.
@@ -23,12 +88,35 @@ async def main() -> None:
     # The user proxy agent is used to get user input after each step of the web surfer.
     # NOTE: you can skip input by pressing Enter.
     user_proxy = UserProxyAgent("user_proxy")
+
+    #mcp api(adapter_ㅁㅁ)와 ai 에이전트 연결
+    agent = AssistantAgent(
+        name="naver_mcp",
+        model_client=model_client,
+        tools=[
+            adapter_weather, 
+            adapter_blog,
+            adapter_news, 
+            adapter_book,
+            adapter_book_adv,
+            adapter_encyc,
+            adapter_cafe,
+            adapter_kin,
+            adapter_local,
+            adapter_spelling,
+            adapter_webkr,
+            adapter_image,
+            adapter_shop,
+            adapter_doc
+            ],
+        system_message="You are a helpful naver api tools, like search, image, information.",
+    )
+
     # The termination condition is set to end the conversation when the user types 'exit'.
     termination = TextMentionTermination("exit", sources=["user_proxy"])
     # Web surfer and user proxy take turns in a round-robin fashion.
-    team = MagenticOneGroupChat([web_surfer], termination_condition=termination, model_client=model_client)
+    team = MagenticOneGroupChat([agent], termination_condition=termination, model_client=model_client)
     # Start the team and wait for it to terminate.
-    await Console(team.run_stream(task="유튜브에서 카페음악을 검색하고 3번째 영상을 틀어줘"))
+    await Console(team.run_stream(task=command))
     await web_surfer.close()
 
-asyncio.run(main())
